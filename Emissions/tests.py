b6a9e07debe22426ea11ebedc9a881bda3c3af1b
al-vincent <al_vincent@hotmail.com>
2019-01-14 17:44:14 +0000

    Added example (mock-up) unit test

--- Emissions/tests.py
+++ Emissions/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def printer(msg):
+    print("\n" + "-"*(len(msg) + 10))
+    print("Got here #" + msg)
+    print("-"*(len(msg) + 10) + "\n")
+
+# printer("0")
+
+class PlaceholderTest(TestCase):
+    # printer("1")
+    
+    def setUp(self):
+        # printer("2")    
+        self.val = 1
+
+    def failure_test(self):
+        self.assertEqual(self.val, 3)
+    
+    def runTest(self):
+        self.failure_test()
