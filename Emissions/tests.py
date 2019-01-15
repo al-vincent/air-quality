49069d1f326fc5aa092fffbc0256ded69757b8ce
al-vincent <al_vincent@hotmail.com>
2019-01-15 08:11:46 +0000

    Basic view now returns minimal HTML

--- Emissions/tests.py
+++ Emissions/tests.py
@@ -1,16 +1,27 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from Emissions.views import index
 
 
 class HomePageTest(TestCase):
     
-    def root_url_resolves_to_home_page(self):
+    def root_url_resolves_to_index_view(self):
         # this will find the function that maps to the "/" route
         found = resolve("/")
         # this tests whether the function for "/" is the 'imdex' function
         # (I assert it should be)
         self.assertEqual(found.func, index)
     
+    def index_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>Air Quality</title>", html)
+        self.assertTrue(html.endswith("</html>"))
+
     def runTest(self):
-        self.root_url_resolves_to_home_page()
+        self.root_url_resolves_to_index_view()
+        self.index_returns_correct_html()
