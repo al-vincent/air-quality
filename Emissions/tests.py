f396ad0c9cf94bb5b87f990de0333cce9ce5c536
al-vincent <al_vincent@hotmail.com>
2019-01-17 08:20:39 +0000

    Refactored home-page test to use template

--- Emissions/tests.py
+++ Emissions/tests.py
@@ -6,22 +6,10 @@ from Emissions.views import index
 
 
 class HomePageTest(TestCase):
-    
-    def root_url_resolves_to_index_view(self):
-        # this will find the function that maps to the "/" route
-        found = resolve("/")
-        # this tests whether the function for "/" is the 'imdex' function
-        # (I assert it should be)
-        self.assertEqual(found.func, index)
-    
+
     def index_returns_correct_html(self):
-        request = HttpRequest()
-        response = index(request)
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>Air Quality</title>", html)
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "index.html")
 
     def runTest(self):
-        self.root_url_resolves_to_index_view()
         self.index_returns_correct_html()
