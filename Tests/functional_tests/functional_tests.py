4e1c0141369016ad5b5a5925d4174331f9f9de46
al-vincent <al_vincent@hotmail.com>
2019-01-21 17:17:11 +0000

    Updated functional tests to check for existence of Leaflet map, plus 3 x drop-down menus (and default values)

--- Tests/functional_tests/functional_tests.py
+++ Tests/functional_tests/functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import unittest
 import time
 
@@ -28,47 +29,63 @@ class NewVisitorTest(unittest.TestCase):
         map = self.browser.find_element_by_id("map")
         # NOTE: need more than this. Can I test if a Leaflet map is actually displayed?
         # E.g. check against the class? [I assume Leaflet maps have a particular class]
-        self.assertEqual(map.__class__, "leaflet-container")
+        self.assertIn("leaflet-container", map.get_attribute("class"))
 
         # He sees a drop-down menu to choose an emissions type to view. There is 
         # a default value, of carbon monoxide
         emissions_menu = self.browser.find_element_by_id("list-emissions")
-        self.assertEqual(
-            emissions_menu.get_attribute("value"), "carbon monoxide"
-        )
+        # check that the element is a select element (i.e. drop-down)
+        Select(emissions_menu) 
+        # get the first option in the list
+        option = emissions_menu.find_element_by_tag_name("option")
+        # test for both the 'value' attribute and the 'text' attribute
+        # (not required, but useful syntax!)
+        self.assertEqual( option.get_attribute("value"), "carbon-monoxide" )
+        self.assertEqual( option.get_attribute("text"), "Carbon Monoxide" )
 
         # He changes this to nitrogen dioxide, and the map updates to show the 
         # new values
-        emissions_menu.send_keys("Nitrogen Dioxide")
+        # emissions_menu.send_keys("Nitrogen Dioxide")
 
-        # Cornelius sees a search box [? or drop-down?] for selecting an area of London. 
-        # He types in "Croydon"
+        # Cornelius sees a drop-down for selecting an area of London. The default value
+        # is "All"
         geographic_menu = self.browser.find_element_by_id("list-london-areas")
-        geographic_menu.send_keys("Croydon")
-        time.sleep(1)   # the page will refresh; this 'enforced wait' allows it to render
+        # check that the element is a select element
+        Select(geographic_menu)
+        # get the first option in the list
+        option = geographic_menu.find_element_by_tag_name("option")
+        self.assertEqual( option.get_attribute("value"), "all" )
+        self.assertEqual( option.text, "All" )
+
+        # He types in "barnet"        
+        # geographic_menu.send_keys("barnet")
+        # time.sleep(1)   # the page will refresh; this 'enforced wait' allows it to render
 
         # The map re-renders to show this area, at a zoom level of <...??...>
 
         # Cornelius sees a selection box that shows different types of illness. 
         # The default value is <...??...>.  
         illness_menu = self.browser.find_element_by_id("list-illnesses")
-        self.assertEqual(
-            illness_menu.get_attribute("Asthma"), "Asthma"
-        )
-
+        # check that the element is a select element
+        Select(illness_menu)
+        # get the first option in the list
+        option = illness_menu.find_element_by_tag_name("option")
+        self.assertEqual( option.get_attribute("value"), "asthma" )
+        self.assertEqual( option.text, "Asthma" )
+        
         # Information about this illness is shown further down the page.
         illness_info = self.browser.find_element_by_id("info-illness")
         self.assertEqual(
-            illness_info.get_attribute("...??..."), "Info about the default illness"
+            illness_info.text, "Info about the default illness"
         )
 
         # He changes the illness to <...??...>, and the information updates.
-        illness_menu.send_keys("Emphesema")
-        time.sleep(1)
-        illness_info = self.browser.find_element_by_id("info-illness")
-        self.assertEqual(
-            illness_info.get_attribute("...??..."), "Info about the updated illness"
-        )
+        # illness_menu.send_keys("Emphesema")
+        # time.sleep(1)
+        # illness_info = self.browser.find_element_by_id("info-illness")
+        # self.assertEqual(
+        #     illness_info.get_attribute("...??..."), "Info about the updated illness"
+        # )
 
         # add a test fail, to remind us to finish the tests
         self.fail("Finish the test")
