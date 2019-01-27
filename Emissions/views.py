9146e15230bee59b58976e3ce620876a449c0442
al-vincent <al_vincent@hotmail.com>
2019-01-27 13:00:05 +0000

    Refactored so that drop-down values are provided to index.html from services.py via views.py

--- Emissions/views.py
+++ Emissions/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . import services
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    data = services.SetupData()
+    return render(request, "index.html", { "emission_types": data.emission_types(),
+                                           "area_groups": data.area_groups(),
+                                           "illness_types": data.illness_types() })
