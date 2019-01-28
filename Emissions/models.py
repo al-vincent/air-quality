5ce359f8316449093c947cf34f77776060834678
al-vincent <al_vincent@hotmail.com>
2019-01-28 07:38:36 +0000

    Added models for Species and Group

--- Emissions/models.py
+++ Emissions/models.py
@@ -3,3 +3,18 @@ from django.db import models
 # Create your models here.
 class Species(models.Model):
     name = models.TextField(default='')
+    code = models.CharField(max_length=4)
+    description = models.TextField(default='')
+    health_effect = models.TextField(default='')
+    link = models.TextField(default='')
+
+    def __str__(self):
+        return f"{self.name} ({self.code}), {self.description}"
+
+class Group(models.Model):
+    name = models.TextField(default='')
+    description = models.TextField(default='')
+    link = models.TextField(default='')
+
+    def __str__(self):
+        return f"{self.name}, {self.description} ({self.link})"
