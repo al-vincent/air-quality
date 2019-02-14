c1bc829819ad6d7f114d8e0e560a713e4790d8ca
al-vincent <al_vincent@hotmail.com>
2019-02-14 18:52:08 +0000

    Added population script, populated db models from API

--- Emissions/models.py
+++ Emissions/models.py
@@ -6,7 +6,7 @@ class Species(models.Model):
     code = models.CharField(max_length=4, default='')
     description = models.TextField(default='')
     health_effect = models.TextField(default='')
-    link = models.TextField(default='')
+    link = models.URLField(default='')
 
     def __str__(self):
         return f"{self.name} ({self.code}), {self.description}"
@@ -14,7 +14,7 @@ class Species(models.Model):
 class Group(models.Model):
     name = models.TextField(default='')
     description = models.TextField(default='')
-    link = models.TextField(default='')
+    link = models.URLField(default='')
 
     def __str__(self):
         return f"{self.name}, {self.description} ({self.link})"
--- populate_airquality.py
+++ populate_airquality.py
@@ -1,42 +1,79 @@
+# setting these environment variables before anything is run is *essential*!
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirQuality.settings')
+
+import django
+django.setup()
+from Emissions.models import Group, Species
+
 import requests
+from sys import exit
+
 
 class PopulateDb():
+    """
+    Main class to populate the sqlite database. Add new methods to populate other
+    tables as required.
+    """
     def populate_groups(self):
+        """
+        Get Group (i.e. location) data from the LondonAir API and create a Group
+        object for each item in the dict that's returned.
+
+        If the API doesn't return a valid response, print a message to the console
+        """
         data = self.get_data_from_API("/Information/Groups/Json")
         if data is not None:
             for item in data["Groups"]["Group"]:
-                grp = Group.object.create(name=item["@GroupName"],
-                                          description=item["@Description"],
-                                          link=item["@WebsuteURL"])
+                grp = Group.objects.get_or_create(name=item["@GroupName"],
+                                                description=item["@Description"],
+                                                link=item["@WebsiteURL"])[0]
                 grp.save()
+        else:
+            print("LondonAir API returned a 404 error for Groups")
     
     def populate_species(self):
+        """
+        Get Species (i.e. emission) data from the LondonAir API and create a Species
+        object for each item in the dict that's returned.
+
+        If the API doesn't return a valid response, print a message to the console
+        """
         data = self.get_data_from_API("/Information/Species/Json")
         if data is not None:
             for item in data["AirQualitySpecies"]["Species"]:
-                species = Species.object.create(name=item["@SpeciesName"],
-                                                code=item["@SpciesCode"],
-                                                description=item["@Description"],
-                                                health_effect=item["@HealthEffect"],
-                                                link=item["@Link"])
+                species = Species.objects.get_or_create(name=item["@SpeciesName"],
+                                                    code=item["@SpeciesCode"],
+                                                    description=item["@Description"],
+                                                    health_effect=item["@HealthEffect"],
+                                                    link=item["@Link"])[0]
                 species.save()
+        else:
+            print("LondonAir API returned a 404 error for Species")
     
     def get_data_from_API(self, url):
-        root = "http://api.erg.kcl.ac.uk/AirQuality"
-        response = requests.get(root + url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            return None
+        """
+        Get data from the LondonAir API. If a connection cannot be made, print 
+        a message to the console and exit.
+        """
+        root = "https://api.erg.kcl.ac.uk/AirQuality"
+        try:
+            response = requests.get(root + url)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return None
+        except requests.exceptions.ConnectionError:
+            print("No response from LondonAir API server")
+            exit(1)       
 
-def main():    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirQuality.settings')
-    from Emissions.models import Group, Species
+def main(): 
+    """
+    Driver function
+    """
     pd = PopulateDb()
     pd.populate_groups()
     pd.populate_species()
-
 
 if __name__ == "__main__":
     main()
