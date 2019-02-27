a40344c98c96b385fa5a86ec55bb5199fcda685d
al-vincent <al_vincent@hotmail.com>
2019-02-27 19:55:05 +0000

    Updated index.js to capture, display user location. Updated index.html to include responsive elements.

--- static/js/index.js
+++ static/js/index.js
@@ -1,25 +1,44 @@
 "use strict";
 
-console.log("Emissions data:")
-console.log(JSON.parse(document.getElementById('geo-data').textContent));
-
 // lat, lng, intensity for different types of emissions
 const EMISSION_TYPES = JSON.parse(document.getElementById('geo-data').textContent)
-const DEFAULT_EMISSIONS = "CO";
+// default values for drop-down menus
+const DEFAULT_EMISSIONS = "CO"; 
 const DEFAULT_ILLNESS = "asthma";
 
+// ***************************************************************************************************
+// Helper functions 
+// ***************************************************************************************************
 function getEmissionsValues(emissionsType){
     return EMISSION_TYPES.map( function(elem) {        
         return [elem["lat"], elem["lng"], elem[emissionsType]];
     });
 }
 
+// ***************************************************************************************************
+// Main 
+// ***************************************************************************************************
 function main(){
+    // Location functions
+    function onLocationFound(pos) {
+        const radius = pos.accuracy / 2;
+        L.marker(pos.latlng)
+            .bindPopup("You are within " + radius + " metres of this point").openPopup()
+            .addTo(map);
+
+        L.circle(pos.latlng, radius).addTo(map);
+    }
+
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
 	// create the map using the travelMap ID tag. Set the (lat, lon) render-centre, and initial zoom level
-	let map = L.map("map", {
-		center: [-37.9, 175.475], 
-		zoom: 16
-        });
+	let map = L.map("map", { zoom: 16 });
+    // get the user's position
+    map.locate({ setView: true});
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
 
     let heat = null;
         	
@@ -31,20 +50,15 @@ function main(){
 			}).addTo(map);
     
     // add a series of points ot the map, with randomly-generated intensities
-    // console.log(getEmissionsValues(DEFAULT_EMISSIONS));
     heat = L.heatLayer(getEmissionsValues(DEFAULT_EMISSIONS), 
                             {radius: 15, blur: 5}).addTo(map); 
     console.log(heat);
-    
+
     const emissionsList = document.getElementById('list-emissions');
     emissionsList.addEventListener("change", function(){
         const newEmissionsType = document.getElementById('list-emissions').value;
-        console.log("New emissions type:");
-        console.log(newEmissionsType);
-        // get the new intensities (need to check via console!)
+        // get the new intensities
         const newEmissions = getEmissionsValues(newEmissionsType);
-        console.log("New emissions values:");
-        console.log(newEmissions);
         // remove the existing points from the map
         heat.remove();
         // plot the new points
