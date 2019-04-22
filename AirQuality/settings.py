e4c247b97fe753870c5a0163a9664163bfcc651b
al-vincent <al_vincent@hotmail.com>
2019-04-22 20:17:03 +0100

    Thinned list of requirements, made small change to STATIC settings

--- AirQuality/settings.py
+++ AirQuality/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')  # html templates
-STATIC_DIR = os.path.join(BASE_DIR, 'static')       # js, css, imgs etc.
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')       # js, css, imgs etc.
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')         # media files
 
 # Quick-start development settings - unsuitable for production
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,10 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # list of *project* locations where static files are stored
-STATICFILES_DIRS = [STATIC_DIR, ]
+# STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),       # js, css, imgs etc.
+    )
 
 # Media files (often updated, incl images)
 MEDIA_ROOT = MEDIA_DIR
