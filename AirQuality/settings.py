8aabf2da73776e0a5312dbf099e73fdc4fc92f1e
al-vincent <al_vincent@hotmail.com>
2019-04-22 14:29:18 +0100

    Changed settings.py

--- AirQuality/settings.py
+++ AirQuality/settings.py
@@ -78,21 +78,24 @@ WSGI_APPLICATION = 'AirQuality.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres', 
-        'HOST': 'testdb',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': 'de16fqejugtlpn',
+        },
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres', 
+#         'HOST': 'testdb',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
