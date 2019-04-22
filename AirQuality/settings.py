b83dfdc142d8ec6f66fb4da1ace647de34c168e8
al-vincent <al_vincent@hotmail.com>
2019-04-22 20:32:58 +0100

    updated settings.py

--- AirQuality/settings.py
+++ AirQuality/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # list of *project* locations where static files are stored
