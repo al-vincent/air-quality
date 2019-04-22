11c62865666195ae2a355866934b0c971017c8cf
al-vincent <al_vincent@hotmail.com>
2019-04-22 16:33:22 +0100

    Updated setting.py with STATIC_ROOT environment variable

--- AirQuality/settings.py
+++ AirQuality/settings.py
@@ -82,9 +82,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': 'de16fqejugtlpn',
-        },
     }
 }
 # DATABASES = {
@@ -133,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # list of *project* locations where static files are stored
 STATICFILES_DIRS = [STATIC_DIR, ]
